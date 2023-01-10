d8fc1b1e8164bae71ab9a126c03d7b662a1ea9f7
NiteshKamble <kamblenitesh351@gmail.com>
2023-01-10 23:32:50 +0530

    stack navigator added

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import home from './src/screen/home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* //Add Stack here */}
-      <Text>Hello World !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name='home'  component={home} />
+    </Stack.Navigator>
+    // <View style={styles.container}>
+    //   {/* //Add Stack here */}
+    //   <Text>Hello World !</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
